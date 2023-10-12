355871691fc09c210b3129a3c4c21dcb7b00d684
Jakub Hoepfner <98704474+Kabooshki@users.noreply.github.com>
2023-10-12 12:06:23 +0100

    Added comments to lexer.py for better readability

--- lexer.py
+++ lexer.py
@@ -1,8 +1,12 @@
+#The Lexer class is meant to use all stages in Lexical analysis.
+#Firstly, it stores any potential characters it may come across as shown below.
+#Then, it takes a 'text' parameter to represent the input in the shell and tokenises all characters
 class Lexer:
     digits = "0123456789"
     operations = "+-/*%"
     space = " "
     
+    #This is the constructor for the Lexer class
     def __init__(self, text):
         self.text = text
         self.index = 0
@@ -10,6 +14,7 @@ class Lexer:
         self.currentChar = self.text[self.index]
         self.token = None
 
+    #This method is used to tokenise all the characters found in text
     def tokenize(self):
         while self.index < len(self.text):
             if self.currentChar in Lexer.digits:
@@ -27,6 +32,7 @@ class Lexer:
             
         return self.tokens
     
+    #This method checks for a '.' character in a number to determine whether to keep it as an integer or change it to float
     def extract_number(self):
         number = ""
         isFloat = False
@@ -38,11 +44,14 @@ class Lexer:
         
         return Integer(number) if not isFloat else Float(number)
     
+    #This method is used throughout the code, to move to the next character in the user input or 'text'
     def next(self):
         self.index += 1
         if self.index < len(self.text):
             self.currentChar = self.text[self.index]
 
+#The token class represents every token created from every character found.
+#The below classes inherit the Token class and specify what type of Token the character is.
 class Token:
     def __init__(self, type, value):
         self.type = type
