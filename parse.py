4041594fe83aff82865a2a9acc0c09a145226fda
Jakub Hoepfner <98704474+Kabooshki@users.noreply.github.com>
2023-10-12 11:53:49 +0100

    Imported Parser to shell.py for BIDMAS

--- parse.py
+++ parse.py
@@ -11,7 +11,22 @@ class Parser:
     def term(self):
         left_node = self.factor()
         self.next()
-        output = left_node 
+        while self.token.value == "*" or self.token.value == "/":
+            operation = self.token
+            self.next()
+            right_node = self.factor()
+            self.next()
+            left_node = [left_node, operation, right_node]
+        return left_node
+    
+    def expression(self):
+        left_node = self.term()
+        while self.token.value == "+" or self.token.value == "-":
+            operation = self.token
+            self.next()
+            right_node = self.term()
+            left_node = [left_node, operation, right_node]
+        return left_node
 
     def parse(self):
         return self.expression()
--- shell.py
+++ shell.py
@@ -1,8 +1,13 @@
 from lexer import Lexer
+from parse import Parser
 
 while True:
     userInput = input("HoepfnerScript: ")
+    
     tokenizer = Lexer(userInput)
     tokens = tokenizer.tokenize()
     
-    print(tokens)
+    parser = Parser(tokens)
+    tree = parser.parse()
+    
+    print(tree)
